5c14b6e99930f65f2484ce6fe83add1ec919ee8d
saecki <tobiasschmitz2001@gmail.com>
2020-06-20 16:46:34 +0200

    updated tests
    Former-commit-id: 64bfc3f3c70e70c6f189aa0181f0edeb689dddf3

--- src/test.rs
+++ src/test.rs
@@ -1,7 +1,11 @@
 use crate::Tag;
+use std::fs;
 
 #[test]
-fn test_sample() {
-    let tag_sample = Tag::read_from_path("./test/sample.m4a").unwrap();
-    println!("{:?}", tag_sample);
+fn test_sample_files() {
+    for f in fs::read_dir("./test/").unwrap() {
+        let filename: String = f.unwrap().path().to_str().unwrap().into();
+        let tag_sample = Tag::read_from_path(&filename).unwrap();
+        println!("{}:\n{:?}", &filename, tag_sample);
+    }
 }
