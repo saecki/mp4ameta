d8190c2482da8a8e17947b1f4e3ad983f2295f4a
saecki <tobiasschmitz2001@gmail.com>
2020-06-20 17:46:29 +0200

    fixed bug with empty typed data throwing an error and some cleanup
    Former-commit-id: aab9367b7e781700fd0c39dc4ce69dffc44a9bdf

--- src/test.rs
+++ src/test.rs
@@ -1,11 +1,27 @@
 use crate::Tag;
 use std::fs;
 
+const EXTENSIONS: [&str; 4] = [".m4a", ".m4b", ".m4p", ".m4v"];
+
 #[test]
 fn test_sample_files() {
     for f in fs::read_dir("./test/").unwrap() {
         let filename: String = f.unwrap().path().to_str().unwrap().into();
+
+        let mut mp4file = false;
+        for e in EXTENSIONS.iter() {
+            if filename.ends_with(e) {
+                mp4file = true;
+                break;
+            }
+        }
+
+        if !mp4file {
+            continue;
+        }
+
+        println!("{}:", &filename);
         let tag_sample = Tag::read_from_path(&filename).unwrap();
-        println!("{}:\n{:?}", &filename, tag_sample);
+        println!("{:#?}", tag_sample);
     }
 }
