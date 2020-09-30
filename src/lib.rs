dbc6953ffda744f4e24fcb2a1f9efdbaf20b9b09
Saecki <tobiasschmitz2001@gmail.com>
2020-09-30 22:48:29 +0200

    documentation example

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,16 @@
 //! A library for reading and writing iTunes style MPEG-4 audio metadata.
+//!
+//! # Example
+//!
+//! ```no_run
+//! let mut tag = mp4ameta::Tag::read_from_path("music.m4a").unwrap();
+//!
+//! println!("{}", tag.artist().unwrap());
+//!
+//! tag.set_artist("artist");
+//!
+//! tag.write_to_path("music.m4a").unwrap();
+//! ```
 #![warn(missing_docs)]
 
 #[macro_use]
